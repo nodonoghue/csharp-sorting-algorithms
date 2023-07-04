3353dfef041992d8864d9bcec3e7683816438162
nodonoghue <nodonoghue@protonmail.com>
2023-07-04 11:15:48 -0700

    Add comments
    Added a brief explanation of what the bubble sort does in both
    ascending and descending order sorts.  Removed some missed
    whitespaces.

--- sorting-algorithms/Bubble/Sort.cs
+++ sorting-algorithms/Bubble/Sort.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace sorting_algorithms.Bubble
 {
+    /// <summary>
+    /// The bubble sort works through the use of a nested loop.  The outer loop iterates through each item in the array (n) to
+    /// use the inner loop to iterate through n - i items in the array, starting at the left.  As the inner loop runs it checks
+    /// if the current item in the array is greater (asending order) or smaller (descending order) than the next item in the array.
+    /// If the comparison operator is true these values are swapped.  This continues until the end of the run in the inner loop,
+    /// this will put the largest (ascending) or smallest (descending) number in the current iteration in the last place to the
+    /// right on each run.  As the name suggests, the largers number (ascending order) or smallest (descending order) will 
+    /// bubble up to the top (right) of the array for each run.
+    /// </summary>
     public static class Sort
     {
         /// <summary>
@@ -20,9 +29,6 @@ namespace sorting_algorithms.Bubble
             for (int i = 0; i < inAry.Length - 1; i++)
             {
                 swapped = false;
-                //The inner loop iterated over the entire loop, minus the number of tiems the outer loop as run.
-                //This is because the highest numbers should bubble to top - i position each time, resulting in
-                //The list being ordered smallest to largest.
                 for (int j = 0; j < inAry.Length - 1 - i; j++)
                 {
                     if (inAry[j] > inAry[j + 1])
@@ -34,7 +40,6 @@ namespace sorting_algorithms.Bubble
                     }
                 }
                 if (swapped == false) break;
-
             }
             return inAry;
         }
@@ -62,7 +67,6 @@ namespace sorting_algorithms.Bubble
                     }
                 }
                 if (swapped == false) break;
-
             }
             return inAry;
         }
