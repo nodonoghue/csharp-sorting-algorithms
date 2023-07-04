c81345e2ad79204c62e3b8313ac013dbb517751e
nodonoghue <nodonoghue@protonmail.com>
2023-07-03 18:44:45 -0700

    Restructured solution
    Added a folder for Bubble in preparation for adding mroe
    sorting algorithms in the future.

---
+++ sorting-algorithms/Bubble/Sort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting_algorithms.Bubble
+{
+    public static class Sort
+    {
+        public static int[] Ascending(int[] inAry)
+        {
+            bool swapped;
+            int swapValue;
+            for (int i = 0; i < inAry.Length - 1; i++)
+            {
+                swapped = false;
+                //The inner loop iterated over the entire loop, minus the number of tiems the outer loop as run.
+                //This is because the highest numbers should bubble to top - i position each time, resulting in
+                //The list being ordered smallest to largest.
+                for (int j = 0; j < inAry.Length - 1 - i; j++)
+                {
+                    if (inAry[j] > inAry[j + 1])
+                    {
+                        swapped = true;
+                        swapValue = inAry[j];
+                        inAry[j] = inAry[j + 1];
+                        inAry[j + 1] = swapValue;
+                    }
+                }
+                if (swapped == false) break;
+
+            }
+            return inAry;
+        }
+    }
+}
--- sorting-algorithms/Program.cs
+++ sorting-algorithms/Program.cs
@@ -8,26 +8,6 @@ int[] inAry = new int[10] { 9, 3, 6, 4, 8, 12, 54, 23, 1, 5 };
 foreach (int i in inAry) Console.Write(i.ToString() + ", ");
 Console.WriteLine();
 
-bool swapped;
-int swapValue;
-for (int i = 0; i < inAry.Length - 1; i++)
-{
-    swapped = false;
-    //The inner loop iterated over the entire loop, minus the number of tiems the outer loop as run.
-    //This is because the highest numbers should bubble to top - i position each time, resulting in
-    //The list being ordered smallest to largest.
-    for (int j = 0; j < inAry.Length - 1 - i; j++)
-    {
-        if (inAry[j] > inAry[j+1])
-        {
-            swapped = true;
-            swapValue = inAry[j];
-            inAry[j] = inAry[j+1];
-            inAry[j+1] = swapValue;
-        }
-    }
-    if (swapped == false) break;
+int[] result = sorting_algorithms.Bubble.Sort.Ascending(inAry);
 
-}
-
-foreach (int i in inAry) Console.Write(i.ToString() + ", ");
+foreach (int i in result) Console.Write(i.ToString() + ", ");
