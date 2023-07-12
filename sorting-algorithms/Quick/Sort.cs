7d63bc4393ff2c9633e973b22f3d6a8f6eaf7ffd
nodonoghue <nodonoghue@protonmail.com>
2023-07-11 23:01:02 -0700

    Quick sort description
    Explanatory comment expanded on to give more
    information on the needs of the algorithm to refresh my
    memory when I get to this next.

--- sorting-algorithms/Quick/Sort.cs
+++ sorting-algorithms/Quick/Sort.cs
@@ -11,6 +11,11 @@ namespace sorting_algorithms.Quick
     /// The array is iterated through putting all elements larger to the right of the pivote and
     /// all elements smaller to the left of the pivot, placing the pivot in the correct spot.
     /// Recursively rinse and repeat until all elements have been put into the correct places.
+    /// This is done by splitting the array on the pivot into partitions, each partition is 
+    /// sent through the pivot to partition code.  Speed depends on pivot point selection,
+    /// the median of the array is the best, but needs to be balanced between adding too much
+    /// logic surrounding pivot point selection.  General tutorials tend to lean towards taking
+    /// the right most element as the pivot point in each iteration.
     /// </summary>
     internal class Sort : Interfaces.ISort
     {
